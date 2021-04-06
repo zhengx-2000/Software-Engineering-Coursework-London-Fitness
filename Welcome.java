4fc45cbd2a50431d08e5b43ffec428df6624c139
zhengx-2000 <jp2018212779@qmul.ac.uk>
2021-04-06 22:07:26 +0800

    version 1.2

--- Welcome.java
+++ Welcome.java
@@ -4,19 +4,28 @@ import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+ * A simple GUI for the welcome page(main page)
+ * @author Xiao Zheng
+ * @version 1.2
+ * @since 4/6/2021
+ */
 public class Welcome extends JFrame implements ActionListener {
     private JFrame jFrame;
     private JLabel videoLabel, liveLabel, membershipLabel;
-    private JButton gallery, videoBtn, liveBtn, membershipBtn;
-    final int WIDTH = 475;
-	final int HEIGHT = 625;
+    private JButton gallery, searchBtn, videoBtn, liveBtn, membershipBtn;
     private JMenuItem accountItem, membershipItem, helpItem;
+    private JTextField search;
+    private final int WIDTH = 475;
+	private final int HEIGHT = 625;
 
+    /**
+     * The constructor of Welcome.java
+     */
     public Welcome() {
         //Font f = new Font("Times New Roman", Font.ITALIC+Font.BOLD, 30);
 		Font btn = new Font("Georgia", Font.BOLD, 13);
         Border labelBorder = new EmptyBorder(0, 30, 0, 30);
-		//setTitle("Welcome to London Fitness!"); 
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int sw = screenSize.width;
@@ -25,33 +34,29 @@ public class Welcome extends JFrame implements ActionListener {
         jFrame.setBounds((sw - WIDTH) / 2, (sh - HEIGHT) / 2, WIDTH, HEIGHT);
 	    jFrame.setSize(WIDTH, HEIGHT);
         jFrame.setResizable(false);
+        jFrame.getContentPane().setBackground(Color.WHITE);
         jFrame.setLayout(new FlowLayout());
         
-        
-	    //container.setLayout(new BoxLayout());
-
-        /*JButton loginBtn = new JButton("Login / My account");
-        JButton membershipBtn = new JButton("Membership");
-        JButton helpBtn = new JButton("help");*/
         Box menuBox = Box.createHorizontalBox();
         JMenuBar menuBar = new JMenuBar();
         accountItem = new JMenuItem("My Account");
         accountItem.setName("myAccount");
         accountItem.setFont(btn);
+        accountItem.setBackground(Color.WHITE);
         accountItem.addActionListener(this);
         membershipItem = new JMenuItem("Membership");
         membershipItem.setName("membership");
         membershipItem.setFont(btn);
+        membershipItem.setBackground(Color.WHITE);
         membershipItem.addActionListener(this);
         helpItem = new JMenuItem("Help");
         helpItem.setName("help");
         helpItem.setFont(btn);
         helpItem.setHorizontalAlignment(SwingConstants.RIGHT);
+        helpItem.setBackground(Color.WHITE);
         helpItem.addActionListener(this);
         menuBar.add(accountItem);
         menuBar.add(membershipItem);
-        //menuBar.add(Box.createHorizontalStrut(100));
-        //menuBar.add(helpItem);
         menuBox.add(menuBar);
         menuBox.add(Box.createHorizontalStrut(200));
         menuBox.add(helpItem);
@@ -60,7 +65,7 @@ public class Welcome extends JFrame implements ActionListener {
 
         JPanel galleryPanel = new JPanel();
         gallery = new JButton();
-        gallery.setBounds(0, 0, 450, 100);
+        gallery.setBounds(0, 0, 450, 200);
         gallery.setBorder(new EmptyBorder(0,0,0,0));
         ImageIcon icon1 = new ImageIcon("image/gallery.jpg"); 
         Image temp1 = icon1.getImage().getScaledInstance(gallery.getWidth(), gallery.getHeight(), Image.SCALE_DEFAULT); 
@@ -69,13 +74,25 @@ public class Welcome extends JFrame implements ActionListener {
         gallery.setContentAreaFilled(false);
         gallery.setBorderPainted(false);
         gallery.addActionListener(this);
+        galleryPanel.setBackground(Color.WHITE);
         galleryPanel.add(gallery);
-        //gallery.setFocusPainted(false);
-        //gallery.setIcon(new ImageIcon(getClass().getResource("cover2.jpg")));
-        //gallery.setPreferredSize(new Dimension(40, 50));
+
+        JPanel searchPanel = new JPanel();
+        search = new JTextField(15);
+        search.setPreferredSize(new Dimension(0, 25));
+        search.setFont(btn);
+        searchBtn = new JButton("Search");
+        searchBtn.setFont(btn);
+        //searchBtn.setContentAreaFilled(false);
+        searchBtn.setBackground(new Color(242, 215, 146));
+        searchBtn.setForeground(Color.WHITE);
+        searchBtn.addActionListener(this);
+        searchPanel.setBackground(Color.WHITE);
+        searchPanel.add(search);
+        searchPanel.add(searchBtn);
 
         Box videoBox = Box.createHorizontalBox();
-        videoLabel = new JLabel("Digital Workout Videos");
+        videoLabel = new JLabel("Digital Workout Video");
         videoLabel.setFont(btn);
         videoLabel.setBorder(labelBorder);
         videoBox.add(videoLabel);
@@ -83,8 +100,9 @@ public class Welcome extends JFrame implements ActionListener {
 
         JPanel videoPanel = new JPanel();
         videoBtn = new JButton();
-        videoBtn.setBounds(0, 0, 450, 100);
-        videoBtn.setBorder(new EmptyBorder(0,0,0,0));
+        //videoBtn.setBounds(0, 0, 450, 85);
+        videoBtn.setBounds(0, 0, 450, 70);
+        videoBtn.setBorder(new EmptyBorder(0, 0, 0, 0));
         ImageIcon icon2 = new ImageIcon("image/video.jpg"); 
         Image temp2 = icon2.getImage().getScaledInstance(videoBtn.getWidth(), videoBtn.getHeight(), Image.SCALE_DEFAULT); 
         icon2 = new ImageIcon(temp2);
@@ -92,6 +110,7 @@ public class Welcome extends JFrame implements ActionListener {
         videoBtn.setContentAreaFilled(false);
         videoBtn.setBorderPainted(false);
         videoBtn.addActionListener(this);
+        videoPanel.setBackground(Color.WHITE);
         videoPanel.add(videoBtn);
 
         Box liveBox = Box.createHorizontalBox();
@@ -103,8 +122,9 @@ public class Welcome extends JFrame implements ActionListener {
 
         JPanel livePanel = new JPanel();
         liveBtn = new JButton();
-        liveBtn.setBounds(0, 0, 450, 100);
-        liveBtn.setBorder(new EmptyBorder(0,0,0,0));
+        //liveBtn.setBounds(0, 0, 450, 85);
+        liveBtn.setBounds(0, 0, 450, 70);
+        liveBtn.setBorder(new EmptyBorder(0, 0, 0, 0));
         ImageIcon icon3 = new ImageIcon("image/live.jpg"); 
         Image temp3 = icon3.getImage().getScaledInstance(liveBtn.getWidth(), liveBtn.getHeight(), Image.SCALE_DEFAULT); 
         icon3 = new ImageIcon(temp3);
@@ -112,6 +132,7 @@ public class Welcome extends JFrame implements ActionListener {
         liveBtn.setContentAreaFilled(false);
         liveBtn.setBorderPainted(false);
         liveBtn.addActionListener(this);
+        livePanel.setBackground(Color.WHITE);
         livePanel.add(liveBtn);
 
         Box membershipBox = Box.createHorizontalBox();
@@ -123,8 +144,9 @@ public class Welcome extends JFrame implements ActionListener {
 
         JPanel membershipPanel = new JPanel();
         membershipBtn = new JButton();
-        membershipBtn.setBounds(0, 0, 450, 100);
-        membershipBtn.setBorder(new EmptyBorder(0,0,0,0));
+        //membershipBtn.setBounds(0, 0, 450, 85);
+        membershipBtn.setBounds(0, 0, 450, 70);
+        membershipBtn.setBorder(new EmptyBorder(0, 0, 0, 0));
         ImageIcon icon4 = new ImageIcon("image/membership.jpg"); 
         Image temp4 = icon4.getImage().getScaledInstance(membershipBtn.getWidth(), membershipBtn.getHeight(), Image.SCALE_DEFAULT); 
         icon4 = new ImageIcon(temp4);
@@ -132,11 +154,13 @@ public class Welcome extends JFrame implements ActionListener {
         membershipBtn.setContentAreaFilled(false);
         membershipBtn.setBorderPainted(false);
         membershipBtn.addActionListener(this);
+        membershipPanel.setBackground(Color.WHITE);
         membershipPanel.add(membershipBtn);
 
         vBox.add(menuBox);
-        //vBox.add(Box.createVerticalStrut(10));
         vBox.add(galleryPanel);
+        vBox.add(Box.createVerticalStrut(5));
+        vBox.add(searchPanel);
         vBox.add(Box.createVerticalStrut(5));
         vBox.add(videoBox);
         vBox.add(videoPanel);
@@ -153,73 +177,63 @@ public class Welcome extends JFrame implements ActionListener {
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    /**
+     * Actions involved in the page.
+     * @param e different actions
+     */
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == accountItem)
-        {
+        if(e.getSource() == accountItem) {
             //System.out.println("myAccount");
             jFrame.setVisible(false);
             new IDPage().init();
         }
-        if(e.getSource() == membershipItem)
-        {
+
+        if(e.getSource() == membershipItem) {
             //System.out.println("membershipItem");
             jFrame.setVisible(false);
             new membershipGUI().init();
         }
-        if(e.getSource() == helpItem)
-        {
+
+        if(e.getSource() == helpItem) {
             //System.out.println("helpItem");
             jFrame.setVisible(false);
             new HelpPage().init();
         }
-        if(e.getSource() == gallery)
+
+        if(e.getSource() == gallery) {
             System.out.println("gallery");
-        if(e.getSource() == videoBtn)
-        {
+        }
+
+        if(e.getSource() == searchBtn) {
+            String text = search.getText();
+            if(text.equals("")) {
+                System.out.println("No enter!");
+            }
+            else {
+                System.out.println(text);
+            }
+        }
+
+        if(e.getSource() == videoBtn) {
             //System.out.println("videoBtn");
             jFrame.setVisible(false);
             new digitalWorkOutVideosPage();
         }
-        if(e.getSource() == liveBtn)
-        {
+
+        if(e.getSource() == liveBtn) {
             //System.out.println("liveBtn");
             jFrame.setVisible(false);
             new LivePersonalTrainingPage();
         }
-        if(e.getSource() == membershipBtn)
-        {
+
+        if(e.getSource() == membershipBtn) {
             //System.out.println("membershipBtn");
             jFrame.setVisible(false);
             new membershipGUI().init();
         }
     }
 
-    /*public class testbtn extends JButton {
-
-        public testbtn(String label) {
-            super(label);  //调用父类构造函数
-            setContentAreaFilled(false);   //不自行绘制按钮背景
-            setPreferredSize(new Dimension(50,30));
-        }
-    
-    //绘制圆和标签
-        protected void paintComponent(Graphics g) {
-           if (getModel().isArmed()) {  //鼠标点击时
-                g.setColor(Color.lightGray);  //颜色为灰色
-            } else {
-                g.setColor(getBackground());  //置按钮颜色
-            }
-            g.fillOval(0, 0, getSize().width, getSize().height);  //绘制圆
-            super.paintComponent(g);  //调用父类函数,绘制其余部分
-        }
-        //绘制边框
-        protected void paintBorder(Graphics g) {
-        g.setColor(getForeground());  //设置边框颜色
-        g.drawOval(0, 0, getSize().width-1, getSize().height-1);  //在边界上绘制一个椭圆
-        }
-    }*/
     public static void main(String[] args) {
         new Welcome();
     }
-
 }
