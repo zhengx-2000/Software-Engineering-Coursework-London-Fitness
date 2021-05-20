3033e620b8eb5fe953cabc1fff7f4d7a160bd68a
zhengx-2000 <jp2018212779@qmul.ac.uk>
2021-05-20 20:16:19 +0800

    Update Javadoc of GetID.java

--- GetID.java
+++ GetID.java
@@ -1,5 +1,16 @@
 import java.io.*;
+
+/**
+ * A control class which could read the ID from ID.txt
+ * @author Xiao Zheng
+ * @version 1.0
+ * @since 5/7/2021
+ */
 public class GetID {
+    /**
+     * A simple method to get ID.
+     * @return The ID stored in ID.txt
+     */
     public String getID() {
         String fileName = "./file/ID.txt";
         String contents = "";
